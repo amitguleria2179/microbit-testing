be1e21ba1dd1692b5ac3731a10f43b0d3ec64de7
amitguleria2179 <amitguleriamlzs2179@gmail.com>
2025-04-07 18:24:13 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showString("MY NAME IS AMIT")
+music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.InBackground)
+basic.forever(function () {
+	
+})
